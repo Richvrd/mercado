a05059eb2a74a3a77c5f66a4b9793de7b7a85265
Ricardo Poblete <ricardo.poblete@alu.ucm.cl>
2022-11-03 02:06:15 -0300

    Actualización Perfil y Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -30,12 +30,61 @@
     </nav>
     <!-- Fin barra navegador -->
 
-    <div>
-        <h1 class="text-center">login</h1>
-    </div>
+    <form class="container" style="width: 20rem;margin-top:5rem;">
+        <h1 class="text-center" style="margin-bottom: 2rem;">Iniciar Sesión</h1>
+        <!-- Email input -->
+        <div class="form-outline mb-2">
+            <input type="email" id="form2Example1" class="form-control" />
+            <label class="form-label" for="form2Example1">Rut</label>
+        </div>
+
+        <!-- Password input -->
+        <div class="form-outline mb-2">
+            <input type="password" id="form2Example2" class="form-control" />
+            <label class="form-label" for="form2Example2">Contraseña</label>
+        </div>
+
+        <!-- 2 column grid layout for inline styling -->
+        <div class="row mb-4">
+            <div class="col d-flex justify-content-center">
+            <!-- Checkbox -->
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
+                <label class="form-check-label" for="form2Example31"> Recordarme </label>
+            </div>
+            </div>
+
+            <div class="col">
+            <!-- Simple link -->
+            <a href="#!">¿Olvidaste tu Contraseña?</a>
+            </div>
+        </div>
+
+        <!-- Submit button -->
+        <button type="button" class="btn btn-primary btn-block mb-4" style="margin-left: 6rem;">Ingresar</button>
+
+        <!-- Register buttons -->
+        <div class="text-center" style="margin-bottom: 2rem;">
+            <p>¿No estás Registrado? <a href="#!">Registrarse</a></p>
+            <p>o inicia sesión con:</p>
+            <button type="button" class="btn btn-link btn-floating mx-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="30" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                </svg>
+            </button>
+
+            <button type="button" class="btn btn-link btn-floating mx-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-google" width="30" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M17.788 5.108a9 9 0 1 0 3.212 6.892h-8" />
+                </svg>
+            </button>
+        </div>
+    </form>
 
     <!-- Footer -->
-<footer class="text-center text-lg-start text-white" style="background-color: #1c2331">
+    <footer class="text-center text-lg-start text-white" style="background-color: #1c2331">
     <!-- Section: Social media -->
     <section class="d-flex justify-content-between p-4" style="background-color: #fd7e14">
         <!-- Left -->
--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -30,9 +30,116 @@
     </nav>
     <!-- Fin barra navegador -->
 
-    <div>
-        <h1 class="text-center">Perfil</h1>
-    </div>
+    <section class="py-5 my-5">
+		<div class="container">
+			<h1 class="mb-5">Perfil</h1>
+			<div class="bg-white shadow rounded-lg d-block d-sm-flex">
+				<div class="profile-tab-nav border-right p-md-5">
+					<div class="p-4">
+						<div class="img-circle text-center mb-3">
+							<img src="imagenes/user.png" alt="Image" class="shadow" style="width: 10rem;">
+						</div>
+						<h4 class="text-center">Kiran Acharya</h4>
+					</div>
+					<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+						<a class="nav-link active" id="account-tab" data-toggle="pill" href="#account" role="tab" aria-controls="account" aria-selected="true">
+							<i class="fa fa-home text-center mr-3"></i> 
+							Cuenta
+						</a>
+						<a class="nav-link" id="password-tab" data-toggle="pill" href="#passwordd" role="tab" aria-controls="password" aria-selected="false">
+							<i class="fa fa-key text-center mr-5"></i> 
+							Password
+						</a>
+						
+					</div>
+				</div>
+				<div class="tab-content p-4 p-md-5" id="v-pills-tabContent">
+					<div class="tab-pane fade show active" id="account" role="tabpanel" aria-labelledby="account-tab">
+						<h3 class="mb-4">Account Settings</h3>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+								  	<label>Nombre</label>
+								  	<input type="text" class="form-control" value="Kiran">
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+								  	<label>Apellido</label>
+								  	<input type="text" class="form-control" value="Acharya">
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+								  	<label>Correo</label>
+								  	<input type="text" class="form-control" value="kiranacharya287@gmail.com">
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+								  	<label>Telefono</label>
+								  	<input type="text" class="form-control" value="+91 9876543215">
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+								  	<label>Empresa</label>
+								  	<input type="text" class="form-control" value="Kiran Workspace">
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+								  	<label>Productos</label>
+								  	<input type="text" class="form-control" value="UI Developer">
+								</div>
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+								  	<label>Descripción</label>
+									<textarea class="form-control" rows="4"></textarea>
+								</div>
+							</div>
+						</div>
+						<div>
+							<button class="btn btn-primary mt-2">Actualizar</button>
+							<button class="btn btn-light mt-2">Cancelar</button>
+						</div>
+					</div>
+					<div class="tab-pane fade" id="passwordd" role="tabpanel" aria-labelledby="password-tab">
+						<h3 class="mb-4">Password Settings</h3>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+								  	<label>Old password</label>
+								  	<input type="password" class="form-control">
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+								  	<label>New password</label>
+								  	<input type="password" class="form-control">
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+								  	<label>Confirm new password</label>
+								  	<input type="password" class="form-control">
+								</div>
+							</div>
+						</div>
+						<div>
+							<button class="btn btn-primary">Update</button>
+							<button class="btn btn-light">Cancel</button>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</section>
+
 
     <!-- Footer -->
 <footer class="text-center text-lg-start text-white" style="background-color: #1c2331">
